180594c5166889e2faeb7fdbf1f99347e6b62933
Soham <rockstarsam1234@gmail.com>
2024-04-27 22:13:30 +0530

    Fixed UI

--- talk-to-genie-fe/src/components/ProcessingSpinner.js
+++ talk-to-genie-fe/src/components/ProcessingSpinner.js
@@ -2,7 +2,7 @@ import { Spinner } from "@blueprintjs/core"
 
 const ProcessingSpinner = ({}) => {
     return (
-        <div className="d-flex fw" style={{ gap: "10px", justifyContent: "center" }}>
+        <div className="d-flex fw" style={{ gap: "10px", justifyContent: "center", alignItems: "center" }}>
             <Spinner intent="success"/>
             <span> Processing your request, please wait! </span>
         </div>
